0006b39a8544f259b2165fb7a64f20dd6c9b21b3
Özer Kutlu ÜNAL <ozer@unal-group.com>
2022-09-20 03:42:52 +0300

    Aranacak sayı değişti

--- src/java2022_HW1/SayiBulma.java
+++ src/java2022_HW1/SayiBulma.java
@@ -5,7 +5,7 @@ public class SayiBulma {
 	public static void main(String[] args) {
 		
 		int[] sayilar = new int[] {1,2,5,7,9,0};
-		int aranacak = 6;
+		int aranacak = 8;
 		boolean varMi = false;
 		
 		for (int sayi : sayilar) {
